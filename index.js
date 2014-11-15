cc382df90b6d1e3cb6a4f3b9ace1b4b35ec0a2f6
babofitos <tehchoi@gmail.com>
2014-11-15 16:51:05 -0500

    change into a module

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
 var request = require('request');
 var cheerio = require('cheerio');
 var article = '';
-var nhkUrl = process.argv[2];
-var makeFile = process.argv[3];
-var fs = require('fs');
 
-request(nhkUrl,
-  function (err, res, body) {
-    if (!err) {
-      var $ = cheerio.load(body);
-      var numberOfParagraphs = $('#newsarticle').children().length;
-      var newsTitle = $('#newstitle').text();
+module.exports = function (nhkUrl, cb) {
+  request(nhkUrl,
+    function (err, res, body) {
+      if (!err) {
+        var $ = cheerio.load(body);
+        var numberOfParagraphs = $('#newsarticle').children().length;
+        var newsTitle = $('#newstitle').text();
 
-      $('#newsarticle').children().each(function (index, element) {
-        
-        var p = $(this);
-        p.contents().each(function (i, e) {
-          var pChild = $(this);
+        $('#newsarticle').children().each(function (index, element) {
+          
+          var p = $(this);
+          p.contents().each(function (i, e) {
+            var pChild = $(this);
 
-          article += extractText($, pChild, '', '');
-        })
-        
+            article += extractText($, pChild, '', '');
+          })
+          
 
-        if (index == numberOfParagraphs - 1) {
-          console.log(article);
-          if (makeFile) {
-            fs.writeFile(newsTitle + '.txt', article, function (err) {
-              if (err) throw err;
-              console.log('Saved file ' + newsTitle + '.txt')
-            })
+          if (index == numberOfParagraphs - 1) {
+            cb(article);
           }
-        }
 
-        //add newline per paragraph but ignore weird empty p nodes
-        if (p.children().length >0) {
-          article += '\n';
-        }
-      })
+          //add newline per paragraph but ignore weird empty p nodes
+          if (p.children().length >0) {
+            article += '\n';
+          }
+        })
+      }
     }
-  })
+  )
+}
 
 //get relevant text nodes, ignore the furigana
 function extractText($, node, part, whole) {
